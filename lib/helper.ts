9f162878b3a84383a02fd6d2e2291d03b8b0ef56
toka yasser <t.yasser@studio50s.com>
2022-12-13 20:46:49 +0200

    change local host

--- lib/helper.ts
+++ lib/helper.ts
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = "http://localhost:8888/.netlify";
 
 export const getUsers = async () => {
   const response = await fetch(`${BASE_URL}/api/users`);
